fd77d8531cada35428fd682eee87426b349d201d
Matthew Shorvon <m.shorvon@gmail.com>
2025-12-09 13:11:55 +0000

    added script to work out why pac-map is slow on the cluster

--- run_all.sh
+++ run_all.sh
@@ -3,7 +3,7 @@ DATETIME=$(date +"%d-%m-%Y %H:%M:%S")
 # python experiment_scripts/make_queries_and_evids.py -dp 20-datasets -all -q 0.1 -e 0.1
 # python experiment_scripts/benchmark.py -m MP AMP MS PACMAP --no-learn --data-path 20-datasets -all -q 0.1 -e 0.1 -dt "$DATETIME"
 # python experiment_scripts/plot_results.py -q 0.1 -e 0.1 -dn 20-datasets -dt "$DATETIME"
-python experiment_scripts/make_queries_and_evids.py -dp small_datasets -all -q 0.1 -e 0.9
+# python experiment_scripts/make_queries_and_evids.py -dp small_datasets -all -q 0.1 -e 0.9
 python experiment_scripts/benchmark.py -m MP AMP MS PACMAP PACMAP-H PACMAP-TopK --no-learn --data-path small_datasets -all -q 0.1 -e 0.9 -dt "$DATETIME"
 python experiment_scripts/plot_results.py -q 0.1 -e 0.9 -dn small_datasets -dt "$DATETIME"
 
@@ -12,7 +12,7 @@ python experiment_scripts/plot_results.py -q 0.1 -e 0.9 -dn small_datasets -dt "
 # python experiment_scripts/make_queries_and_evids.py -dp 20-datasets -all -q 0.25 -e 0.25
 # python experiment_scripts/benchmark.py -m MP AMP MS PACMAP --no-learn --data-path 20-datasets -all -q 0.25 -e 0.25 -dt "$DATETIME"
 # python experiment_scripts/plot_results.py -q 0.25 -e 0.25 -dn 20-datasets -dt "$DATETIME"
-python experiment_scripts/make_queries_and_evids.py -dp small_datasets -all -q 0.25 -e 0.75
+# python experiment_scripts/make_queries_and_evids.py -dp small_datasets -all -q 0.25 -e 0.75
 python experiment_scripts/benchmark.py -m MP AMP MS PACMAP PACMAP-H PACMAP-TopK --no-learn --data-path small_datasets -all -q 0.25 -e 0.75 -dt "$DATETIME"
 python experiment_scripts/plot_results.py -q 0.25 -e 0.75 -dn small_datasets -dt "$DATETIME"
 
@@ -21,6 +21,6 @@ python experiment_scripts/plot_results.py -q 0.25 -e 0.75 -dn small_datasets -dt
 # python experiment_scripts/make_queries_and_evids.py -dp 20-datasets -all -q 0.4 -e 0.4
 # python experiment_scripts/benchmark.py -m MP AMP MS PACMAP --no-learn --data-path 20-datasets -all -q 0.4 -e 0.4 -dt "$DATETIME"
 # python experiment_scripts/plot_results.py -q 0.4 -e 0.4 -dn 20-datasets -dt "$DATETIME"
-python experiment_scripts/make_queries_and_evids.py -dp small_datasets -all -q 0.4 -e 0.6
+# python experiment_scripts/make_queries_and_evids.py -dp small_datasets -all -q 0.4 -e 0.6
 python experiment_scripts/benchmark.py -m MP AMP MS PACMAP PACMAP-H PACMAP-TopK --no-learn --data-path small_datasets -all -q 0.4 -e 0.6 -dt "$DATETIME"
 python experiment_scripts/plot_results.py -q 0.4 -e 0.6 -dn small_datasets -dt "$DATETIME"
