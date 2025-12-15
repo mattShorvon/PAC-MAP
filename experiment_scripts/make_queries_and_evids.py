59f4ae9617389b9d823b950c042dff059bc7dbe0
Matthew Shorvon <m.shorvon@gmail.com>
2025-12-15 14:25:23 +0000

    added check in make_queries_evid.py to make sure randomly generated evidence is not zero probability and won't cause divide by 0 error later

--- experiment_scripts/make_queries_and_evids.py
+++ experiment_scripts/make_queries_and_evids.py
@@ -5,6 +5,8 @@ import os
 import sys
 from pathlib import Path
 import random
+from spn.io.file import from_file
+from spn.utils.evidence import Evidence
 
 # EXAMPLE: python make_queries_and_evids.py -dp benchmark_datasets -all -q 0.4 -e 0.4
 # Check command line args and initialise variables
@@ -69,6 +71,16 @@ for dataset in datasets:
             print(f"Error type: {type(error).__name__}")
             continue
     print(f"Number of features: {num_features}")
+
+    # Load the spn, to be used for making sure the random evidence is not 0 prob
+    try:
+        spn = from_file(Path(f"{data_path}/{dataset}/{dataset}.spn"))
+        print(f"SPN loaded: {spn.vars()} vars and {spn.arcs()} arcs")
+    except FileNotFoundError as error:
+        print(".spn file not found, check the file path or that the spn exists")
+        print(error)
+        print("moving on to the next dataset")
+
     lines = []
     for i in range(num_queries):
         num_q_vars = np.floor(num_features * q_percent)
@@ -79,8 +91,18 @@ for dataset in datasets:
             num_e_vars = 1
         q_var_ids = random.sample(range(num_features), int(num_q_vars))
         remaining = [i for i in range(num_features) if i not in q_var_ids]
-        e_var_ids = random.sample(remaining, min(int(num_e_vars), len(remaining)))
-        e_values = random.choices([0, 1], k=len(e_var_ids))
+        test_evid_prob = 0.0
+        evidences_tried = 0
+        while test_evid_prob == 0.0:
+            e_var_ids = random.sample(remaining, min(int(num_e_vars), len(remaining)))
+            e_values = random.choices([0, 1], k=len(e_var_ids))
+            test_evid = Evidence()
+            for i, var in enumerate(e_var_ids):
+                test_evid[spn.scope()[var]] = [e_values[i]]
+            test_evid_prob = spn.value(test_evid)
+            print(f"Attempt No: {evidences_tried}")
+            evidences_tried += 1
+        print(f"Found non-zero prob evidence after {evidences_tried} attempts")
         e_line = ' '.join(
             [f"{var_id} {val}" for var_id, val in zip(e_var_ids, e_values)]
         )
