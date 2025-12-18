b65c48e791c6b753f51a1cfa89c6265b83b3010b
Matthew Shorvon <m.shorvon@gmail.com>
2025-12-18 14:45:09 +0000

    added the saving to file_path in learn_spns script

--- experiment_scripts/learn_spns.py
+++ experiment_scripts/learn_spns.py
@@ -4,55 +4,53 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 from spn.actions.learn import Learn
+from spn.data.partitioned_data import PartitionedData
 from spn.learn import gens
+from spn.io.file import to_file
 import argparse
 
-parser = argparse.ArgumentParser(description='MAP Benchmark Experiment Params')
-parser.add_argument('-all','--all-datasets', action='store_true',
-                    help="Iterate through all dataset folders" \
-                    "(containing queries, evidences and spns) in the specified"\
-                    "data path, or just use specific ones?")
-parser.add_argument('-dp', '--data-path', default='test_inputs', 
-                    help='Path to folder with all subfolders of spns, ' \
-                    'queries and evidence in them')
-parser.add_argument('-d', '--datasets', nargs='+',
-                    help='Dataset names separated by space (e.g., iris nltcs)')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='MAP Benchmark Experiment Params')
+    parser.add_argument('-all','--all-datasets', action='store_true',
+                        help="Iterate through all dataset folders" \
+                        "(containing queries, evidences and spns) in the specified"\
+                        "data path, or just use specific ones?")
+    parser.add_argument('-dp', '--data-path', default='test_inputs', 
+                        help='Path to folder with all subfolders of spns, ' \
+                        'queries and evidence in them')
+    parser.add_argument('-d', '--datasets', nargs='+',
+                        help='Dataset names separated by space (e.g., iris nltcs)')
 
-args = parser.parse_args()
-use_all = args.all_datasets
-data_path = args.data_path
-if use_all:
-    datasets = sorted(
-        [folder.name for folder in os.scandir(data_path) if folder.is_dir()]
-    )
-else:
-    datasets = args.datasets
-
-print(f"Datasets: {datasets}")
-
-for dataset in datasets:
-    # Set up the SPN
-    print(f"Running benchmark on dataset {dataset}")
-    print("Learning SPN ...")
-    kclusters = 3
-    pval = 0.5
-    try:
-        # Try the first pattern
-        data = PartitionedData(
-            Path(f"{data_path}/{dataset}/{dataset}-train.data"), 1.0
+    args = parser.parse_args()
+    use_all = args.all_datasets
+    data_path = args.data_path
+    if use_all:
+        datasets = sorted(
+            [folder.name for folder in os.scandir(data_path) if folder.is_dir()]
         )
-    except:
-        # If that fails, try the second pattern
+    else:
+        datasets = args.datasets
+
+    print(f"Datasets: {datasets}")
+
+    for dataset in datasets:
+        # Set up the SPN
+        print(f"Running benchmark on dataset {dataset}")
+        print("Learning SPN ...")
+        kclusters = 3
+        pval = 0.5
         try:
+            # Try the first pattern
             data = PartitionedData(
                 Path(f"{data_path}/{dataset}/{dataset}.train.data"), 1.0
             )
         except Exception as error:
             print(f"Failed to load training data: {error}")
             print(f"Error type: {type(error).__name__}")
-            run_success = False
             continue 
-        
-    spn = gens(data.scope, data.training_data, kclusters, pval)
-    spn.root = True
-    print("SPN learned:", spn.vars(), "vars and", spn.arcs(), "arcs")
+            
+        spn = gens(data.scope, data.training_data, kclusters, pval, numeric=False)
+        spn.root = True
+        print("SPN learned:", spn.vars(), "vars and", spn.arcs(), "arcs")
+        path = Path(f"{data_path}/{dataset}/{dataset}.spn")
+        to_file(spn, path)
